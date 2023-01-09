e226b7306d1ee43c7f9748b8e854a62ab8015d98
César Mejía <iconfidence@gmail.com>
2023-01-09 14:32:46 -0600

    Some changes

--- web/pages/LandingPage.js
+++ web/pages/LandingPage.js
@@ -1,3 +1,9 @@
+import PropTypes from "prop-types";
+import NextSeo from "next-seo";
+import groq from "groq";
+import imageUrlBuilder from "@sanity/image-url";
+import client from "../client";
+
 import Host from "../components/host";
 import Layout from "../components/layout";
 import List from "../components/list";
@@ -62,7 +68,28 @@ const sponsors = [
   },
 ];
 
-export default function LandingPage({ config }) {
+const builder = imageUrlBuilder(client);
+const pageQuery = groq`
+*[_type == "route" && slug.current == $slug][0]{
+  page-> {
+    ...,
+    content[] {
+      ...,
+      cta {
+        ...,
+        route->
+      },
+      ctas[] {
+        ...,
+        route->
+      }
+    }
+  }
+}
+`;
+
+export default function LandingPage({ config, content }) {
+  console.log(content);
   return (
     <Layout config={config}>
       <Counter />
@@ -95,3 +122,50 @@ export default function LandingPage({ config }) {
     </Layout>
   );
 }
+
+LandingPage.getInitialProps = async function ({ query }) {
+  const { slug } = query;
+  if (!query) {
+    console.error("no query");
+    return;
+  }
+  // Internal
+  if (slug && slug !== "/") {
+    const res = await client.fetch(pageQuery, { slug });
+    return { ...res.page, slug };
+  }
+  // Frontpage
+  if (slug && slug === "/") {
+    const res = client.fetch(groq`
+      *[_id == "global-config"][0] {
+        frontpage -> {
+          ...,
+          content[] {
+            ...,
+            cta {
+              ...,
+              route->
+            },
+            ctas[] {
+              ...,
+              route->
+            }
+          }
+        }
+      }
+    `);
+    return { ...res.frontpage, slug };
+  }
+  return null;
+};
+
+LandingPage.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  // TODO: improve types
+  disallowRobots: PropTypes.any,
+  openGraphImage: PropTypes.any,
+  content: PropTypes.any,
+  config: PropTypes.any,
+  slug: PropTypes.any,
+};
