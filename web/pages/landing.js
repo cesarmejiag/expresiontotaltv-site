8768eef89ead0cea63e5c060a387b085f84f7734
César Mejía <iconfidence@gmail.com>
2023-01-11 11:41:07 -0600

    Create layout.js and layout.module.css

--- web/pages/landing.js
+++ web/pages/landing.js
@@ -1,4 +1,77 @@
-export default function Landing({ config }) {
-  console.log(config);
-  return <div>Landing</div>;
+import PropTypes from "prop-types";
+import NextSeo from "next-seo";
+import groq from "groq";
+import imageUrlBuilder from "@sanity/image-url";
+import client from "../client";
+import Layout from "../components/layout";
+
+const builder = imageUrlBuilder(client);
+const pageQuery = groq`
+*[_type == "route" && slug.current == $slug][0]{
+  page-> {
+    ...,
+    content[] {
+      ...,
+      cta {
+        ...,
+        route->
+      },
+      ctas[] {
+        ...,
+        route->
+      }
+    }
+  }
 }
+`;
+
+export default function Landing({ config }) {
+  return <Layout config={config}>Landing</Layout>;
+}
+
+Landing.getInitialProps = async function ({ query }) {
+  const { slug } = query;
+  if (!query) {
+    console.error("no query");
+    return;
+  }
+  // Internal
+  if (slug && slug !== "/") {
+    const res = await client.fetch(pageQuery, { slug });
+    return { ...res.page, slug };
+  }
+  // Frontpage
+  if (slug && slug === "/") {
+    const res = client.fetch(groq`
+      *[_id == "global-config"][0] {
+        frontpage -> {
+          ...,
+          content[] {
+            ...,
+            cta {
+              ...,
+              route->
+            },
+            ctas[] {
+              ...,
+              route->
+            }
+          }
+        }
+      }
+    `);
+    return { ...res.frontpage, slug };
+  }
+  return null;
+};
+
+Landing.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  // TODO: improve types
+  disallowRobots: PropTypes.any,
+  openGraphImage: PropTypes.any,
+  content: PropTypes.any,
+  config: PropTypes.any,
+  slug: PropTypes.any,
+};
