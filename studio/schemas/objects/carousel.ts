1bf7a531537aa0e10b7233e61719e9c4cd5207cb
César Mejía <iconfidence@gmail.com>
2023-01-26 23:26:21 -0600

    Add configuration to carousel

--- studio/schemas/objects/carousel.ts
+++ studio/schemas/objects/carousel.ts
@@ -2,7 +2,29 @@ export default {
   name: 'carousel',
   title: 'Carousel',
   type: 'object',
+  fieldsets: [
+    {
+      title: 'Carousel options',
+      name: 'options',
+    },
+  ],
   fields: [
+    {
+      name: 'autoplay',
+      description: 'Play automatically, without action from a user.',
+      title: 'Autoplay',
+      type: 'boolean',
+      fieldset: 'options',
+    },
+    {
+      name: 'delay',
+      description: 'Delay between transitions (in seconds).',
+      title: 'Delay',
+      type: 'number',
+      initialValue: 5,
+      fieldset: 'options',
+      validation: (Rule: any) => Rule.required().integer().positive().min(1).max(10),
+    },
     {
       name: 'items',
       title: 'Items',
