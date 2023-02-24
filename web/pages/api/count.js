0337846bb311a91dc8c5ea251deb9ff4c8dc62b0
César Mejía <iconfidence@gmail.com>
2023-02-24 10:12:46 -0600

    Update

--- web/pages/api/count.js
+++ web/pages/api/count.js
@@ -1,3 +1,44 @@
+import { Sequelize, DataTypes } from "sequelize";
+import mysql2 from "mysql2";
+
 export default async function handler(req, res) {
-  res.status(200).json({ success: true, data: { count: 412312 } });
+  // Connect with database.
+  const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: process.env.INSTANCE_HOST,
+      dialect: "mysql",
+      dialectModule: mysql2,
+    }
+  );
+
+  // Define model.
+  const Visit = sequelize.define(
+    "Visit",
+    { count: { type: DataTypes.DOUBLE } },
+    { tableName: "Visits" }
+  );
+
+  // Query last count and update.
+  try {
+    const result = await Visit.findAll({
+      limit: 1,
+      order: [["createdAt", "DESC"]],
+    });
+
+    if (result.length === 0) {
+      throw new Error("Can't find last count.");
+    }
+
+    const visit = result[0];
+    const count = visit.count;
+
+    visit.count = visit.count + 1;
+    await visit.save();
+    res.status(200).json({ success: true, data: { count } });
+  } catch (err) {
+    res.status(200).json({ success: false, message: err.message });
+  }
 }
