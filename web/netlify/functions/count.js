7cbb76ac62d07531c09be2873b4d4dcfc2d486b7
César Mejía <iconfidence@gmail.com>
2023-02-24 00:50:07 -0600

    Change logic of count api to count lambda function

--- web/netlify/functions/count.js
+++ web/netlify/functions/count.js
@@ -1,6 +1,29 @@
+import Visit from "@/db/models/Visit";
+
 exports.handler = async function (event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
-  };
+  try {
+    const result = await Visit.findAll({
+      limit: 1,
+      order: [["createdAt", "DESC"]],
+    });
+
+    if (result.length === 0) {
+      throw new Error("Can't find last count.");
+    }
+
+    const visit = result[0];
+    const count = visit.count;
+
+    visit.count = visit.count + 1;
+    await visit.save();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ success: true, data: { count } }),
+    };
+  } catch (err) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ success: false, message: err.message }),
+    };
+  }
 };
--- web/pages/api/count.js
+++ web/pages/api/count.js
@@ -1,27 +1,3 @@
-import Visit from "@/db/models/Visit";
-
-/* export default async function handler(req, res) {
-  try {
-    const result = await Visit.findAll({
-      limit: 1,
-      order: [["createdAt", "DESC"]],
-    });
-
-    if (result.length === 0) {
-      throw new Error("Can't find last count.");
-    }
-
-    const visit = result[0];
-    const count = visit.count;
-
-    visit.count = visit.count + 1;
-    await visit.save();
-    res.status(200).json({ success: true, data: { count } });
-  } catch (err) {
-    res.status(200).json({ success: false, message: err.message });
-  }
-} */
-
 export default async function handler(req, res) {
   res.status(200).json({
     success: true,
